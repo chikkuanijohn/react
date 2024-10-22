cd7fcea86ddfd759972bedf4a99d0e3d507e5ce8
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-10-22 21:54:33 +0530

    new file

--- todoapp/src/component/TaskList.jsx
+++ todoapp/src/component/TaskList.jsx
@@ -1,69 +1,87 @@
-import React,{ useState,useEffect } from 'react';
-import axios from 'axios';
+import React,{useState,useEffect} from "react";
+import axios from 'axios'
 
-const AddTask = () => {
-    const [tasks,setTasks]=useEffect([]);
+
+const TaskList=()=>{
+    const [tasks,setTasks]=useState([]);
     const [editing,setEditing]=useState(false);
-    const [currentTask,setDescription] = useState({id:null, tittle:''});
+    const [currentTask, setCurrentTask]= useState({id:null, title:'',description:''});
 
-    useEffect( () =>{
-        axios.get('https://aswanth74.pythonantwhere.com/api/tasks/')
+    useEffect(()=>{
+        axios.get('https://aswanth74.pythonanywhere.com/api/tasks/')
         .then(response => setTasks(response.data))
         .catch(error => console.log(error));
     },[]);
-    const editTask =(task)=> {
-         setEditing(true);
-         setCurrentTask(task);
 
+    const editTask=(task)=>{
+        setEditing(true);
+        setCurrentTask(task);
     };
-
-    const updateTask =(id,updatedTask) => {
-         setEditing(false);
-         axios.put('htto://localhost:8000/api/tasks/${id}',updatedTask)
-             .then(response => {
-                 setTasks(tasks.map(task => (task.id ==id ? response.data : task)));
-
-             })
-             .catch(error => console.log(error));
-
-            
-     }
-     return(
-         <div className='container mt-3'>
-             <h2>TaskList</h2>
-             <table className='table table-bordered table-hover'>
-                 {tasks.map(task => (
-                     <tr key={task.id}>
-                         <td>{task.id}</td>
-                         <td>{task.title}</td>
-                         <td>{task.description}</td>
-                        <td><button className='btn btn-warning px-3' onClick={() => editTask(task)}Edit></button></td>
-                        <td><button className='btn btn-danger' onClick={() => deleteTask(task)}>Delete</button></td>
-
-                   </tr>
-
-                  ))}
-
-             </table>
-            {editing ? (
-                 <editTaskForm
+    const updateTask=(id,updatedTask)=>{
+        setEditing(false);
+        axios.put('https://aswanth74.pythonanywhere.com/api/tasks/${id}',updatedTask)
+            .then(response=>{
+            setTasks(tasks.map(task=>(task.id === id? response.data:task)));
+        })
+        .catch(error=>console.log(error));
+    }
+    return(
+        <div className="container mt-3">
+            <h2>Task List</h2>
+            <table className="table table-bordered table-hover">
+                {tasks.map(task => (
+                    <tr key={task.id}>
+                        <td>{task.id}</td>
+                        <td>{task.title}</td>
+                        <td>{task.description}</td>
+                        <td><button className="btn btn-warning px-3" onClick={() =>editTask(task)}>Edit</button></td>
+                        <td><button className="btn btn-danger" onClick={() =>deleteTask(task.id)}>Delete</button></td>
+                    </tr>
+                ))}
+            </table>
+            {editing ?(
+                <EditTaskForm
                  currentTask={currentTask}
                  updateTask={updateTask}
-                 />
-             ) :null}
+                />
+            ):null}
+        </div>
+    );
+};
+ const EditTaskForm=({currentTask,updateTask})=>{
+    const [task,setTask]=useState(currentTask);
 
-        
+     const handelInputChange=(e)=>{
+        const{name,value}=e.target;
+        setTask({...task,[name]:value});
+     };
 
-         </div>
+     const handelSubmit =(e)=>{
+        e.preventDefault();
+        updateTask(task.id,task)
+     };
+     return(
+        <form onSubmit={handelSubmit}>
+            <h2>Edit Task</h2>
+            <div>
+                <label>Title</label>
+                <input
+                type="text"
+                name="title"
+                value={task.title}
+                onChange={handelInputChange}
+                />
+            </div>
+            <div>
+                <label>Description</label>
+                <textarea
+                name="description"
+                value={task.description}
+                onChange={handelInputChange}
+                />
+            </div>
+            <button type="submit">Update Task</button>
+        </form>
      );
-
  };
-
-const editTaskForm = ({ currentTask,updateTask}) => {
-     const [task]
-}
 export default TaskList
-
-
-    
-
